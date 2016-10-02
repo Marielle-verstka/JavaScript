6372600befdaae33674391c2cf6c710bff7c305c
marielle <marielle.verstka@gmail.com>
2016-10-02 21:59:03 +0300

    3-4, 5-6

--- JavaScript_3-4/js/script.js
+++ JavaScript_3-4/js/script.js
@@ -1,3 +1,114 @@
+// // Задаём отступы для body
+// document.body.style.padding = "24px 96px";
+// // Создаём сам объект
+// var pageTest = {
+// // Вводим исходные данные
+// 	title: 'Тест по программированию',
+// 	test: [
+// 		{
+// 		question: 'Вопрос №1',
+// 		answers: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'],
+// 		},
+// 		{
+// 		question: 'Вопрос №2',
+// 		answers: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'],
+// 		},	
+// 		{
+// 		question: 'Вопрос №3',
+// 		answers: ['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3'],
+// 		}				
+// 	],
+// 	control: 'Проверить мои результаты',
+// 	action: '#',
+// 	createForm: function() {
+// 		// Создаём название формы
+//  		var form = document.createElement('form');
+// 		form.setAttribute('action', this.action);
+// 		form.setAttribute('method', 'post');
+// 		document.body.appendChild(form);
+// 	},
+// 	createTitle: function() {
+// 		// Создаём название страницы
+//  		var h1 = document.createElement('h1');
+// 		h1.innerHTML = this.title;
+// 		h1.style.textAlign = "center";
+// 		var form = document.querySelector('form');
+// 		form.appendChild(h1);
+// 	},
+// 	createTest: function() {
+// 		//Создаем сам тест
+// 		for (var i = 0; i < this.test.length; i++) {
+// 			// Перебор вопросов
+// 			var form = document.querySelector('form');
+			
+// 			var fieldset = document.createElement('fieldset');
+// 			fieldset.style.border = "none";
+// 			fieldset.style.margin = "0";
+// 			fieldset.style.padding = "0";
+// 			form.appendChild(fieldset);
+
+// 			var legend = document.createElement('legend');
+// 			legend.innerHTML = i + 1 + '. ' + this.test[i].question;
+// 			legend.style.fontSize = "24px";
+// 			legend.style.margin = "25px 0 10px 0px";
+// 			fieldset.appendChild(legend);
+
+// 			for (var j = 0; j < this.test[i].answers.length; j++) {
+// 			// Перебор ответов
+
+// 		  		var label = document.createElement('label');
+// 		  		label.style.display = "block";
+// 		  		label.style.marginLeft = "25px";
+// 		  		fieldset.appendChild(label);
+
+// 				var span = document.createElement('span');
+// 				span.innerHTML = this.test[i].answers[j];
+// 				span.style.fontSize = '20px';
+// 				label.appendChild(span);
+
+// 				var input = document.createElement('input');
+// 				input.type = 'checkbox';
+// 				var x = i+1;
+// 				var y = j+1;
+// 				var xy = x + "" + y;
+// 				input.setAttribute('value', xy );
+// 				input.setAttribute('id', xy );
+// 				label.insertBefore(input, label.children[0]);
+// 			}
+
+// 		}
+// 	},
+// 	createButton: function() {
+// 		// Создаём кнопку для отправки формы
+// 		var form = document.querySelector('form');
+//  		var submit = document.createElement('input');
+// 		submit.setAttribute('type', 'submit');
+// 		submit.setAttribute('value', this.control);
+// 		submit.style.display = "block";
+// 		submit.style.lineHeight = "65px";
+// 		submit.style.margin = "50px auto";
+// 		submit.style.padding = "0 44px";
+// 		submit.style.backgroundColor = "#d0e2f0";
+// 		submit.style.fontSize = '26px';
+// 		submit.style.cursor = 'pointer';
+// 		form.appendChild(submit);
+// 	},
+// 	createPageTest: function() {
+// 		this.createForm();
+// 		this.createTitle();
+// 		this.createTest();
+// 		this.createButton();
+// 	}
+// }
+
+
+// // pageTest.createForm();
+// // pageTest.createTitle();
+// // pageTest.createTest();
+// // pageTest.createButton();
+// pageTest.createPageTest();
+
+
 // Задаём отступы для body
 document.body.style.padding = "24px 96px";
 // Создаём сам объект
@@ -20,32 +131,38 @@ var pageTest = {
 	],
 	control: 'Проверить мои результаты',
 	action: '#',
+	// createForm: function() {
+	// 	// Создаём название формы
+ // 		var form = document.createElement('form');
+	// 	form.setAttribute('action', this.action);
+	// 	form.setAttribute('method', 'post');
+	// 	document.body.appendChild(form);
+	// },
 	createForm: function() {
 		// Создаём название формы
- 		var form = document.createElement('form');
+		var form = document.createElement('form');
 		form.setAttribute('action', this.action);
 		form.setAttribute('method', 'post');
-		document.body.appendChild(form);
+		document.body.insertBefore(form, document.body.firstChild);
+		return form;
 	},
-	createTitle: function() {
+	createTitle: function(x) {
 		// Создаём название страницы
  		var h1 = document.createElement('h1');
 		h1.innerHTML = this.title;
 		h1.style.textAlign = "center";
-		var form = document.querySelector('form');
-		form.appendChild(h1);
+		x.appendChild(h1);
 	},
-	createTest: function() {
+	createTest: function(x) {
 		//Создаем сам тест
 		for (var i = 0; i < this.test.length; i++) {
 			// Перебор вопросов
-			var form = document.querySelector('form');
 			
 			var fieldset = document.createElement('fieldset');
 			fieldset.style.border = "none";
 			fieldset.style.margin = "0";
 			fieldset.style.padding = "0";
-			form.appendChild(fieldset);
+			x.appendChild(fieldset);
 
 			var legend = document.createElement('legend');
 			legend.innerHTML = i + 1 + '. ' + this.test[i].question;
@@ -78,9 +195,8 @@ var pageTest = {
 
 		}
 	},
-	createButton: function() {
+	createButton: function(x) {
 		// Создаём кнопку для отправки формы
-		var form = document.querySelector('form');
  		var submit = document.createElement('input');
 		submit.setAttribute('type', 'submit');
 		submit.setAttribute('value', this.control);
@@ -91,21 +207,36 @@ var pageTest = {
 		submit.style.backgroundColor = "#d0e2f0";
 		submit.style.fontSize = '26px';
 		submit.style.cursor = 'pointer';
-		form.appendChild(submit);
+		x.appendChild(submit);
 	},
 	createPageTest: function() {
-		this.createForm();
-		this.createTitle();
-		this.createTest();
-		this.createButton();
+		var form = this.createForm();
+		this.createTitle(form);
+		this.createTest(form);
+		this.createButton(form);
 	}
-}
+};
 
 
 // pageTest.createForm();
-// pageTest.createTitle();
+// pageTest.createTitle(form);
 // pageTest.createTest();
 // pageTest.createButton();
 pageTest.createPageTest();
 
 
+
+
+// Dmitriy Vepretskiy
+// В общем неплохо. Единственный совет. Ты в каждом методе ищешь форму. 
+// DOM-элемент можно создать один раз, вернуть его в переменную и передавать ее как аргумент остальным функциям
+// createForm: function() {
+// var form = document.createElement('form');
+// return form;
+// }
+// createPageTest: function() {
+// var form = this.createForm();
+// this.createTitle(form);
+// this.createTest(form);
+// this.createButton(form);
+// }
